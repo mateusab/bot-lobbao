aa29b59193b9d5abc4e66047c1a96670a4ab7f97
mateusab <mateusabantunes@hotmail.com>
2020-10-23 08:49:00 -0300

    add list lobby validations

--- src/services/LobbyService/usecases/ListLobby.ts
+++ src/services/LobbyService/usecases/ListLobby.ts
@@ -1,4 +1,5 @@
-import { Lobby } from './../../../interfaces/LobbyInterface';
+import { BotMessages } from '@utils/BotMessages';
+import { Lobby } from '@interfaces/LobbyInterface';
 import { Injectable } from '@nestjs/common';
 import { Message } from 'discord.js';
 
@@ -6,9 +7,20 @@ import { Message } from 'discord.js';
 export class ListLobby {
     execute(lobbyName: string, message: Message, lobbies: Lobby[]) {
         const lobby = lobbies.find(lobby => lobby.name === lobbyName)
-        message.channel.send(`Lobby: ${lobbyName} - jogadores\n`)
-        lobby.players.map(player => {
-            message.channel.send(`${player.name} (${player.level})`)
-        })
+
+        if (!lobby) {
+            message.channel.send(BotMessages.lobbyDoesNotExists(lobbyName))
+        } else {
+            const spots = 10 - lobby.count 
+            message.channel.send(BotMessages.listLobby(lobbyName, spots))
+            
+            if(spots != 10) {
+                lobby.players.forEach(player => {
+                    message.channel.send(`${player.name} (${player.level})`)
+                })
+            }
+        }
+
+
     }
 }

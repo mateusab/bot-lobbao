02c3e087fc5a3cfe7db58af6e9ebe5f1f509d263
mateusab <mateusabantunes@hotmail.com>
2020-10-23 18:35:57 -0300

    remove count from list lobby

--- src/services/LobbyService/usecases/ListLobby.ts
+++ src/services/LobbyService/usecases/ListLobby.ts
@@ -12,14 +12,11 @@ export class ListLobby {
         if (!lobby) {
             message.channel.send(BotMessages.lobbyDoesNotExists(lobbyName))
         } else {
-            const spots = MAX_LOBBY_PLAYERS - lobby.count 
-            message.channel.send(BotMessages.listLobby(lobbyName, spots))
+            message.channel.send(BotMessages.listLobby(lobbyName))
             
-            if(spots != MAX_LOBBY_PLAYERS) {
-                lobby.players.forEach(player => {
-                    message.channel.send(`${player.name} (${player.level})`)
-                })
-            }
+            lobby.players.forEach(player => {
+                message.channel.send(`${player.name} (${player.level})`)
+            })
         }
 
 
--- src/utils/BotMessages.ts
+++ src/utils/BotMessages.ts
@@ -1,9 +1,9 @@
 export const BotMessages = Object.freeze({
-    listLobby: (lobbyName: string, spots: number) => `Lobby: ${lobbyName} (${spots} vagas)`,
+    listLobby: (lobbyName: string) => `Lobby: ${lobbyName}`,
     lobbyAlreadyExists: (lobbyName: string) => `A lobby **${lobbyName}** já existe!\nDigite **!join ${lobbyName}** para entrar.`,
     lobbyCreated: (lobbyName: string) => `Lobby **${lobbyName}** criada com sucesso! Digite **!join ${lobbyName}** para entrar.`,
     lobbyDoesNotExists: (lobbyName: string) => `A lobby **${lobbyName}** não existe! Digite **!create ${lobbyName}** para criar.`,
-    playerAlreadyInLobby: (lobbyName: string, playerName: string) => `**${message.member.displayName}** já está na lobby **${lobbyName}**.`,
+    playerAlreadyInLobby: (lobbyName: string, playerName: string) => `**${playerName}** já está na lobby **${lobbyName}**.`,
     playerIsNotPartOfLobby: (lobbyName: string, playerName: string) => `${playerName} não faz parte da lobby **${lobbyName}**.`,
     playerJoinnedLobby: (lobbyName: string, playerName: string) => `**${playerName}** entrou na lobby **${lobbyName}**.`,
     playerLeftLobby: (lobbyName: string, playerName: string) => `${playerName} saiu da lobby **${lobbyName}**.`

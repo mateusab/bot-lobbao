a95ed6c7c7102a1ff6e7493d996ea45a4dc0cea4
mateusab <mateusabantunes@hotmail.com>
2020-10-24 14:07:48 -0300

    improve list and add emojis, so cute

--- src/services/LobbyService/usecases/ListLobby.ts
+++ src/services/LobbyService/usecases/ListLobby.ts
@@ -6,20 +6,36 @@ import { Message } from 'discord.js';
 @Injectable()
 export class ListLobby {
     execute(lobbyName: string, message: Message, lobbies: Lobby[]) {
-        const lobby = lobbies.find(lobby => lobby.name === lobbyName)
-
-        if (!lobby) {
-            message.channel.send(BotMessages.lobbyDoesNotExists(lobbyName))
+        if (lobbyName === '') {
+            if (lobbies.length > 0) {
+                const lobbiesOpen: String[] = getOpenLobbies(lobbies)
+                message.channel.send(BotMessages.openedLobbies)
+                message.channel.send(lobbiesOpen)
+                message.channel.send(BotMessages.typeListToSeeSpecificLobby)
+            } else {
+                message.channel.send(BotMessages.noLobbiesOpened)
+            }
         } else {
-            if(lobby.count === 0) {
-                message.channel.send(BotMessages.lobbyEmpty(lobbyName))
-            } else {                
-                message.channel.send(BotMessages.listLobby(lobbyName, lobby.count))
-                const playersAtLobby: String[] = getLobbyPlayers(lobby)
-                message.channel.send(playersAtLobby)
+            const lobby = lobbies.find(lobby => lobby.name === lobbyName)
+    
+            if (!lobby) {
+                message.channel.send(BotMessages.lobbyDoesNotExists(lobbyName))
+            } else {
+                if(lobby.count === 0) {
+                    message.channel.send(BotMessages.lobbyEmpty(lobbyName))
+                } else {                
+                    message.channel.send(BotMessages.listLobby(lobbyName, lobby.count))
+                    const playersAtLobby: String[] = getLobbyPlayers(lobby)
+                    message.channel.send(playersAtLobby)
+                }
             }
         }
     }
+}
+function getOpenLobbies(lobbies: Lobby[]): String[] {
+    return lobbies.map(lobby => {
+        return `**${lobby.name}** - ${lobby.count} jogadores`;
+    });
 }
 
 function getLobbyPlayers(lobby: Lobby): String[] {
